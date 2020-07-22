495f625ae4edae120fd1fcf62e854acaa3d3c1e1
Alin Marin Elena <alin@elena.space>
2020-07-22 20:22:32 +0100

    add rdf coverage

--- tests/test-field.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-import dlpoly as dlp
-
-d = dlp.DLPoly(field="FIELD")
--- tests/test-statis.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from dlpoly.statis import Statis
-
-s = Statis(source="STATIS")
-s.flatten()
