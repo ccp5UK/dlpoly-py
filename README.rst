00d93502eddad7a665fbadc72c4ba8abeaa4a829
Alin Marin Elena <alin@elena.space>
2020-07-27 10:55:54 +0100

    pypi changes

--- README.rst
+++ README.rst
@@ -1,6 +1,7 @@
-
-
 dlpoly-py
----------
+=========
 
+this contains tools to read input and output for DL_POLY
+it can also produce inputs and be mixed with other python packages
+like ASE, MDAnalysis, MDAnse or pymatgen
 
--- dlpoly/__init__.py
+++ dlpoly/__init__.py
@@ -19,7 +19,7 @@ modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [basename(f)[:-3] for f in modules if isfile(f)
            and not f.endswith('__init__.py')]
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 try:
     from .dlpoly import DLPoly
