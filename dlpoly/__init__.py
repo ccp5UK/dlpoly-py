f4bbcc838ebee8f6106728c969b63bef572b9c72
Alin Marin Elena <alin@elena.space>
2020-08-14 09:21:06 +0100

    bump version

--- dlpoly/__init__.py
+++ dlpoly/__init__.py
@@ -19,7 +19,7 @@ modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [basename(f)[:-3] for f in modules if isfile(f)
            and not f.endswith('__init__.py')]
 
-__version__ = '0.0.4'
+__version__ = '0.1.0'
 
 try:
     from .dlpoly import DLPoly
