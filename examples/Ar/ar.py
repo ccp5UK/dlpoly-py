341e9296b80f36a3b09f041f358c16240cdd2693
Alin M Elena <alinm.elena@gmail.com>
2021-04-13 15:30:46 +0100

    add simple argon example

--- examples/Ar/ar.py
+++ examples/Ar/ar.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 from dlpoly import DLPoly
+import os
 
-dlp="/home/drFaustroll/playground/dlpoly/dl-poly-alin/build-mxatms/bin/DLPOLY.Z"
+dlp="/home/drFaustroll/bin/DLPOLY.Z"
 
 dlPoly = DLPoly(control="Ar.control", config="Ar.config",
                 field="Ar.field", workdir="argon")
@@ -12,3 +13,14 @@ dlPoly = DLPoly(control="Ar.control", config="argon/REVCON", destconfig="Ar.conf
                 field="Ar.field", workdir="argon-T310")
 dlPoly.control['temp'] = 310.0
 dlPoly.run(executable=dlp,numProcs = 4)
+
+wkd='argon-neweps'
+field = DLPoly(field="Ar.field").field
+field.vdws[0].params=['125.0','3.0']
+field.write('Ar-n.field')
+
+dlPoly = DLPoly(control="Ar.control", config="argon/REVCON", destconfig="Ar.config",
+                field='Ar-n.field', workdir=wkd)
+
+dlPoly.run(executable=dlp,numProcs = 4)
+
