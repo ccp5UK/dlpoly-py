47addfc20a2302a4a31aa0e96e17907317eb321d
alin m elena <alinm.elena@gmail.com>
2023-01-24 11:12:30 +0000

    update version 0.3.8

--- dlpoly/__init__.py
+++ dlpoly/__init__.py
@@ -26,7 +26,7 @@ modules = Path(__file__).parent.glob("*.py")
 __all__ = [f.stem for f in modules if f.is_file()
            and f.name != '__init__.py']
 
-__version__ = '0.3.7'
+__version__ = '0.3.8'
 
 try:
     from .dlpoly import DLPoly
