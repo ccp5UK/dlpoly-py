626d489e2f52646bfb751fc3dd19aa9924bb844d
apw951 <h.devereux@qmul.ac.uk>
2023-05-10 12:43:55 +0100

    style

--- tests/test_correlations.py
+++ tests/test_correlations.py
@@ -2,6 +2,7 @@
 import unittest
 from dlpoly.correlations import Correlations
 
+
 class CorrelationsTest(unittest.TestCase):
 
     def setUp(self):
@@ -15,31 +16,32 @@ class CorrelationsTest(unittest.TestCase):
     def test_correlations_ncorrelations(self):
         self.assertEqual(self.correlations.n_correlations, 3,
                          'incorrect number of correlations')
-        
+
     def test_correlations_window(self):
         self.assertEqual(self.correlations.averaging_window, [1, 1, 1],
                          'incorrect averaging window parameters')
-    
+
     def test_correlations_blocks(self):
         self.assertEqual(self.correlations.blocks, [1, 1, 1],
                          'incorrect blocks parameters')
-        
+
     def test_correlations_points(self):
         self.assertEqual(self.correlations.points_per_block, [300, 300, 300],
                          'incorrect block points parameters')
-    
+
     def test_correlations_labels(self):
         self.assertEqual(self.correlations.labels, [['vv', 'Ar'], ['vv', 'Kr'], ['ss', 'global']],
                          'incorrect correlation labels')
-        
+
     def test_correlations_lags(self):
-        self.assertEqual(sum(self.correlations.lags[0]), 22.425, 
+        self.assertEqual(sum(self.correlations.lags[0]), 22.425,
                          'incorrect lags checksum')
-        
+
     def test_correlations_components(self):
         self.assertEqual(sum(self.correlations.components[0]['v_x-v_x']), -6.860305158479998,
                          'incorrect component checksum')
-        
+
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(CorrelationsTest('test_correlations_ncorrelations'))
@@ -50,6 +52,7 @@ def suite():
     suite.addTest(CorrelationsTest('test_correlations_lags'))
     suite.addTest(CorrelationsTest('test_correlations_components'))
     return suite
+
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
