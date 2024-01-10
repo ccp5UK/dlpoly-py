dc949212670afe81ba59894ad9e28aa0c9707497
apw951 <h.devereux@qmul.ac.uk>
2024-01-10 16:06:01 +0000

    fix ar-heat

--- examples/Ar-heat/ar.py
+++ examples/Ar-heat/ar.py
@@ -2,7 +2,7 @@
 
 from dlpoly import DLPoly
 
-dlp="/home/drFaustroll/playground/dlpoly/dl-poly-alin/build-mxatms/bin/DLPOLY.Z"
+dlp="/your/home/bin/DLPOLY.Z"
 
 dlPoly = DLPoly(control="Ar.control", config="Ar.config",
                 field="Ar.field", workdir="argon")
@@ -10,7 +10,7 @@ dlPoly.run(executable=dlp,numProcs = 4)
 
 for T in range(200,601,50):
     print("Process T = {}".format(T))
-    dlPoly = DLPoly(control="Ar.control", config="argon/REVCON", destconfig="Ar.config",
+    dlPoly = DLPoly(control="Ar.control", config="Ar.config",
                 field="Ar.field", workdir="argon-T{}".format(T))
-    dlPoly.control['temp'] = T
-    dlPoly.run(executable=dlp,numProcs = 4)
+    dlPoly.control['temperature'] = (T, 'K')
+    dlPoly.run(executable=dlp,numProcs = 1, debug=True)
