5c32c6ef560a0b03fe0299d26ee5087fea968856
alin elena <alin@elena.re>
2024-01-11 13:13:55 +0000

    renamed

--- examples/h2o/water-flexible.py
+++ examples/h2o/water-flexible.py
@@ -5,7 +5,7 @@ from dlpoly import DLPoly
 dlp="/path_to_dlpoly/bin/DLPOLY.Z"
 dlp = "/home/drFaustroll/lavello/build-dlp-jan/bin/DLPOLY.Z"
 
-dlPoly = DLPoly(control="water.control", config="water.config",
-                field="spce.field", workdir="water")
+dlPoly = DLPoly(control="water-flexible.control", config="water.config",
+                field="spce-flexible.field", workdir="water")
 
 dlPoly.run(executable=dlp,numProcs = 1)
