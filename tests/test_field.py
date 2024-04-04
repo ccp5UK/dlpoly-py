7002de8edb50de3c56762495deeeb9c3d165b6a2
Harvey Devereux <h.devereux@qmul.ac.uk>
2024-04-04 07:57:09 +0000

    adds Tersoff (ters/kihs) parsing

--- tests/test_field.py
+++ tests/test_field.py
@@ -7,15 +7,19 @@ class FieldTest(unittest.TestCase):
 
     def setUp(self):
         self.field = FieldTest.field
+        self.field_ters = FieldTest.field_ters
 
     @classmethod
     def setUpClass(cls):
         super(FieldTest, cls).setUpClass()
         cls.field = dlp.DLPoly(field="tests/FIELD").field
+        cls.field_ters = dlp.DLPoly(field="tests/FIELD.ters").field
 
     def test_field_units(self):
         self.assertEqual(self.field.units, "kcal",
-                         'incorrect units')
+                         'incorrect units in FIELD')
+        self.assertEqual(self.field_ters.units, "ev",
+                         'incorrect units in FIELD.ters')
 
     def test_field_mol(self):
         self.assertEqual(self.field.molecules['Gramicidin A'].n_mols, 8,
@@ -23,11 +27,26 @@ class FieldTest(unittest.TestCase):
         self.assertEqual(self.field.molecules['Gramicidin A'].n_atoms, 354,
                          'incorrect number of atoms in gramidicin')
 
+    def test_field_ters(self):
+        self.assertEqual(len(self.field_ters.pots), 5,
+                         'incorrect number of tersoff potentials')
+        self.assertEqual(self.field_ters.pots[('C',)][0].pot_class, 'ters',
+                         'missing tersoff single params')
+        self.assertEqual(self.field_ters.pots[('C', 'C')][0].pot_class, 'ters-cross',
+                         'missing tersoff cross params')
+        self.assertEqual(self.field_ters.nTersoffs, 2,
+                         'incorrect number of Tersoffs potentials')
+        self.assertEqual(self.field_ters.nTersoffCrosses, 3,
+                         'incorrect number of Tersoff cross potentials')
+        self.assertEqual(self.field_ters.nKihss, 0,
+                         'incorrect number of Tersoff KIHS potentials')
+
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(FieldTest('test_field_units'))
     suite.addTest(FieldTest('test_field_mol'))
+    suite.addTest(FieldTest('test_field_ters'))
     return suite
 
 
