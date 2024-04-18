afdf522dd490d0acf2475571b01ec7a31392868f
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2024-04-18 10:41:54 +0100

    Make statis indexable, improve consistency between YAML and old-style

--- tests/test_statis.py
+++ tests/test_statis.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import unittest
+from pathlib import Path
+
 from dlpoly.statis import Statis
 
 
@@ -11,7 +13,7 @@ class StatisTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         super(StatisTest, cls).setUpClass()
-        cls.statis = Statis(source="tests/STATIS")
+        cls.statis = Statis(source=Path(__file__).parent / "STATIS")
 
     def test_statis_ncolumns(self):
         self.assertEqual(self.statis.columns, 68,
@@ -27,50 +29,47 @@ class StatisTest(unittest.TestCase):
                              'incorrect cell time/step')
 
     def test_statis_temperature(self):
-        self.assertEqual(self.statis.data[2, 4], 3.000000E+02,
+        self.assertEqual(self.statis["System Temperature"][2], 3.000000E+02,
                          'incorrect temperature')
 
     def test_statis_pressure(self):
-        self.assertEqual(self.statis.data[3, 29], 1.170673E+01,
+        self.assertEqual(self.statis[29][3], 1.170673E+01,
                          'incorrect pressure')
 
     def test_statis_vpmf(self):
-        self.assertEqual(self.statis.data[1, 28], 0.000000E+00,
+        self.assertEqual(self.statis[28][1], 0.000000E+00,
                          'incorrect virial pmf')
 
     def test_statis_consQ(self):
-        self.assertEqual(self.statis.data[4, 3], 6.565874E+04,
+        self.assertEqual(self.statis[3][4], 6.565874E+04,
                          'incorrect conserved quantity')
 
     def test_statis_energies(self):
-        self.assertListEqual(list(self.statis.data[2, 5:13]),
-                             [float(i) for i in '''6.022616E+03  8.482584E+04
-                             -4.086941E+05  1.121753E+04  3.073748E+05  1.129854E+04
-                             0.000000E+00  6.577677E+04'''.split()],
+        self.assertListEqual(list(self.statis[5:13][2]),
+                             [6.022616E+03, 8.482584E+04, -4.086941E+05, 1.121753E+04,
+                              3.073748E+05, 1.129854E+04, 0.000000E+00, 6.577677E+04],
                              'incorrect energy terms')
 
     def test_statis_virial(self):
-        self.assertListEqual(list(self.statis.data[1, 14:21]),
-                             [float(i) for i in '''-6.506158E+03 -1.808586E+06
-                                 4.084056E+05  3.127625E+05  0.000000E+00
-                                 1.080912E+06  0.000000E+00'''.split()],
+        self.assertListEqual(list(self.statis.data[1, 13:20]),
+                             [-6.506158E+03, -1.808586E+06, 4.084056E+05, 3.127625E+05,
+                              0.000000E+00, 1.080912E+06, 0.000000E+00],
                              'incorrect virial terms')
 
     def test_statis_angles(self):
-        self.assertListEqual(list(self.statis.data[1, 25:28]),
+        self.assertListEqual(list(self.statis.data[1, 24:27]),
                              [90.0, 90.0, 90.0],
                              'incorrect angles')
 
     def test_statis_volume(self):
-        self.assertEqual(self.statis.data[4, 21], 9.993474E+05,
+        self.assertEqual(self.statis.data[4, 20], 9.993474E+05,
                          'incorrect volume')
 
     def test_statis_stress(self):
-        self.assertListEqual(list(self.statis.data[4, 31:40]),
-                             [float(i) for i in '''8.060310E+00 -2.636976E+00
-                              -1.941572E+00 -2.636976E+00  2.316919E+01
-                              4.915622E-01 -1.941572E+00  4.915622E-01
-                              2.080145E+01'''.split()],
+        self.assertListEqual(list(self.statis.data[4, 30:39]),
+                             [8.060310E+00, -2.636976E+00, -1.941572E+00,
+                              -2.636976E+00, 2.316919E+01, 4.915622E-01,
+                              -1.941572E+00, 4.915622E-01, 2.080145E+01],
                              'incorrect stress')
 
 
